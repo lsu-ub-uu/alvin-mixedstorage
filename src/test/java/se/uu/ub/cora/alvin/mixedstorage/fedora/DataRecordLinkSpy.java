7718226a316514f3f9536d83bea7f98f3172d37b
madde <madde@e4c48372b8d3>
2021-03-16 20:06:31 +0000

    Fixing tests

---
+++ src/test/java/se/uu/ub/cora/alvin/mixedstorage/fedora/DataRecordLinkSpy.java
@@ -0,0 +1,231 @@
+/*
+ * Copyright 2021 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.uu.ub.cora.alvin.mixedstorage.fedora;
+
+import java.util.ArrayList;
+/*
+ * Copyright 2021 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.util.Collection;
+import java.util.List;
+
+import se.uu.ub.cora.data.Action;
+import se.uu.ub.cora.data.DataAtomic;
+import se.uu.ub.cora.data.DataAttribute;
+import se.uu.ub.cora.data.DataElement;
+import se.uu.ub.cora.data.DataGroup;
+import se.uu.ub.cora.data.DataRecordLink;
+
+public class DataRecordLinkSpy implements DataRecordLink {
+
+	public String nameInData;
+	public String recordType;
+	public String recordId;
+	public List<DataElement> children = new ArrayList<>();
+	private String repeatId;
+
+	public DataRecordLinkSpy(String nameInData) {
+		this.nameInData = nameInData;
+	}
+
+	public DataRecordLinkSpy(String nameInData, String recordType, String recordId) {
+		this.nameInData = nameInData;
+		this.recordType = recordType;
+		this.recordId = recordId;
+	}
+
+	@Override
+	public void addAction(Action action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Action> getActions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsChildWithNameInData(String nameInData) {
+		for (DataElement dataElement : children) {
+			if (nameInData.equals(dataElement.getNameInData())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void addChild(DataElement dataElement) {
+		children.add(dataElement);
+	}
+
+	@Override
+	public void addChildren(Collection<DataElement> dataElements) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<DataElement> getChildren() {
+		return children;
+	}
+
+	@Override
+	public List<DataElement> getAllChildrenWithNameInData(String nameInData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DataElement> getAllChildrenWithNameInDataAndAttributes(String nameInData,
+			DataAttribute... childAttributes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataElement getFirstChildWithNameInData(String nameInData) {
+		for (DataElement dataElement : children) {
+			if (nameInData.equals(dataElement.getNameInData())) {
+				return dataElement;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String getFirstAtomicValueWithNameInData(String nameInData) {
+		for (DataElement dataElement : children) {
+			if (nameInData.equals(dataElement.getNameInData())) {
+				if (dataElement instanceof DataAtomic) {
+					return ((DataAtomic) dataElement).getValue();
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<DataAtomic> getAllDataAtomicsWithNameInData(String nameInData) {
+		List<DataAtomic> matchingDataAtomics = new ArrayList<>();
+		for (DataElement dataElement : children) {
+			if (nameInData.equals(dataElement.getNameInData())
+					&& dataElement instanceof DataAtomic) {
+				matchingDataAtomics.add((DataAtomic) dataElement);
+			}
+		}
+		return matchingDataAtomics;
+	}
+
+	@Override
+	public DataGroup getFirstGroupWithNameInData(String nameInData) {
+		for (DataElement dataElement : children) {
+			if (nameInData.equals(dataElement.getNameInData())) {
+				if (dataElement instanceof DataGroup) {
+					return ((DataGroup) dataElement);
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<DataGroup> getAllGroupsWithNameInData(String nameInData) {
+		List<DataGroup> matchingDataGroups = new ArrayList<>();
+		for (DataElement dataElement : children) {
+			if (nameInData.equals(dataElement.getNameInData())
+					&& dataElement instanceof DataGroup) {
+				matchingDataGroups.add((DataGroup) dataElement);
+			}
+		}
+		return matchingDataGroups;
+	}
+
+	@Override
+	public Collection<DataGroup> getAllGroupsWithNameInDataAndAttributes(String nameInData,
+			DataAttribute... childAttributes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeFirstChildWithNameInData(String nameInData) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAllChildrenWithNameInData(String nameInData) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAllChildrenWithNameInDataAndAttributes(String nameInData,
+			DataAttribute... childAttributes) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public DataAtomic getFirstDataAtomicWithNameInData(String nameInData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRepeatId(String repeatId) {
+		this.repeatId = repeatId;
+
+	}
+
+	@Override
+	public String getRepeatId() {
+		return repeatId;
+	}
+
+	@Override
+	public String getNameInData() {
+		return nameInData;
+	}
+
+}
